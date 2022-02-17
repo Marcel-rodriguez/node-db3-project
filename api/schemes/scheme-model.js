a535e318bb17cae27d76b6b8c5d6d926a86ed0ed
marcel <marcel_rodrgez@yahoo.com>
2022-02-17 02:44:14 -0500

    finished steps scheme 1

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -25,7 +25,7 @@ const find = async () => { // EXERCISE A
     .groupBy('sc.scheme_id')
 }
 
-function findById(scheme_id) { // EXERCISE B
+async function findById(scheme_id) { // EXERCISE B
   /*
     1B- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`:
 
@@ -91,6 +91,30 @@ function findById(scheme_id) { // EXERCISE B
         "steps": []
       }
   */
+
+      const rows = await db('schemes as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+      .where('sc.scheme_id', scheme_id)
+      .select('st.*', 'sc.scheme_name', 'sc.scheme_id')
+      .orderBy('st.step_number')
+
+      const results = {
+        scheme_id: rows[0].scheme_id,
+        scheme_name: rows[0].scheme_name,
+        steps: []
+      }
+
+      rows.forEach(row => {
+        if (row.step_id) {
+          results.steps.push({
+            step_id: row.step_id,
+            step_number: row.step_number,
+            instructions: row.instructions,
+          })
+        }
+      })
+
+      return results
 }
 
 function findSteps(scheme_id) { // EXERCISE C
